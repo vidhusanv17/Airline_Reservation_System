aea710c29ee838582ef0322ee20eed18820ec401
vidhusanv17 <vidhusan17@gmail.com>
2024-10-26 10:59:57 +0530

    Add files via upload

---
+++ src/login/user_signin.java
@@ -0,0 +1,35 @@
+package login;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import database.DBConnect;
+
+public class user_signin extends DBConnect {
+
+	public void insert() throws SQLException {
+
+		String query = "insert into user(id,username,email,pass) values(?,?,?,?);";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter id");
+		int id = sc.nextInt();
+		System.out.println("enter user name");
+		String name = sc.next();
+		System.out.println("enter email");
+		String email = sc.next();
+		System.out.println("enter password:");
+		String pass = sc.next();
+		PreparedStatement pst = con.prepareStatement(query);
+		pst.setInt(1, id);
+		pst.setString(2, name);
+		pst.setString(3, email);
+		pst.setString(4, pass);
+		pst.execute();
+		System.out.println("User Registered successfully!!!!");
+		
+	}
+
+	
+
+}
